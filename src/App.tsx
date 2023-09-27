085349c07e51b63eb6ac6a57682de3acccd3c6e5
Volodymyr Barybin <vohanchib@gmail.com>
2023-09-27 17:34:16 +0300

    Revert "Revert "created contact block with map""
    This reverts commit 96b60d039baa6f0f96126702dd895d82c0304bde.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
+import Contact from './components/Contact';
 import TheGallery from './components/TheGallery';
 import TheFeatures from './components/TheFeatures';
 import PageWrapper from './components/PageWrapper';
 import TheTechInAction from './components/TheTechInAction';
 
 function App() {
+  const api = process.env.REACT_APP_MAP_API_KEY;
   return (
     <PageWrapper>
       <TheFeatures />
       <TheGallery />
       <TheTechInAction />
+      <Contact />
     </PageWrapper>
   );
 }
--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,7 +1,16 @@
+import Map from './Map';
+import ContactForm from './ContactForm';
 import styles from './index.module.css';
 
 const Contact = () => {
-  return <section></section>;
+  return (
+    <section className={styles.contact}>
+      <div className={styles.container}>
+        <Map />
+        <ContactForm />
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
