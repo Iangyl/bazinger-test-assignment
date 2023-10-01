1a90b7e0e5be03c330f2f8919d7b41c04e4efb7e
Volodymyr Barybin <vohanchib@gmail.com>
2023-10-02 00:38:35 +0300

    added adaptive for tech action block

--- src/components/TheTechInAction/index.tsx
+++ src/components/TheTechInAction/index.tsx
@@ -1,11 +1,16 @@
 import styles from './index.module.css';
 import playIcon from '../../assets/icons/play.svg';
 import { useModal } from '../ModalProvider/ModalProvider';
+import useScreenSize from '../../hooks/useScreenSize';
+import { useMemo } from 'react';
 
 const TheTechInAction = () => {
   const { openModal } = useModal();
+  const { width } = useScreenSize();
+
+  const screen = useMemo(() => width <= 425 ? 'mobile' : 'notmobile', [width]);
   return (
-    <section className={styles.theTechInAction}>
+    <section className={`${styles.theTechInAction} ${styles[screen]}`}>
       <div className={`wrapper ${styles.container}`}>
         <button
           type="button"
