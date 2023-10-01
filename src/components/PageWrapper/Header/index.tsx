44226df4b989a2f34e3b82fd3a7d4c35233c4dbe
Volodymyr Barybin <vohanchib@gmail.com>
2023-10-01 15:01:27 +0300

    added rules for adaptive view for header, footer, contact and 2 simple blocks

--- src/components/PageWrapper/Header/index.tsx
+++ src/components/PageWrapper/Header/index.tsx
@@ -1,20 +1,43 @@
+import { useState } from 'react'; // Import useState
 import { links } from './index.config';
 import Logo from '../../Logo';
 import styles from './index.module.css';
 
-const Header = () => (
-  <header className={styles.header}>
-    <div className={`wrapper ${styles.navContainer}`}>
-      <Logo />
-      <ul className={`list-type-reset ${styles.navbar}`}>
-        {links.map((item, idx) => (
-          <li key={idx} className={styles.navbarItem}>
-            <a className={styles.navbarItemLink} href={item.link}>{item.name}</a>
-          </li>
-        ))}
-      </ul>
-    </div>
-  </header>
-);
+const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <header className={styles.header}>
+      <div className={`wrapper ${styles.navContainer}`}>
+        <Logo />
+        <button
+          className={`${styles.menuButton} ${isMenuOpen ? styles.close : ''}`}
+          onClick={toggleMenu}
+        >
+          <span className={styles.burgerIcon}></span>
+          <span className={styles.burgerIcon}></span>
+          <span className={styles.burgerIcon}></span>
+        </button>
+        <ul
+          className={`list-type-reset ${styles.navbar} ${
+            isMenuOpen ? styles.openMenu : ''
+          }`}
+        >
+          {links.map((item, idx) => (
+            <li key={idx} className={styles.navbarItem}>
+              <a className={styles.navbarItemLink} href={item.link}>
+                {item.name}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
